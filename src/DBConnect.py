341e77b69650835806ccacb5181a628a7eb3d720
Bakanalmi <marc2almi@gmail.com>
2021-03-20 21:24:13 +0100

    Falta afegir excepcions

--- src/DBConnect.py
+++ src/DBConnect.py
@@ -1,8 +1,42 @@
 import pymongo
 
-
 client = pymongo.MongoClient(
-    "mongodb+srv://lluis-vacares:runnerwar@runnerwar.yuhsa.mongodb.net/RunnerWar?retryWrites=true&w=majority")
-#print(client.get_database("RunnerWar").list_collection_names())
+    "mongodb+srv://runnerwar:runnerwar@runnerwar.yuhsa.mongodb.net/RunnerWar?retryWrites=true&w=majority")
 
-#print(client.list_database_names())
+db = client["RunnerWar"]
+
+
+def create_account(email, password, name, faction):
+    col = db["Cuenta"]
+    doc = {
+        "_id": email,
+        "email": "NULL",
+        "password": password,
+        "username": name,
+        "coins": "0",
+        "points": "0",
+        "faction": faction
+    }
+    return col.insert_one(doc)
+
+
+def edit_account(old, new):
+    col = db["Cuenta"]
+    query = {
+        "username": old
+    }
+    new = {
+        "$set":
+            {
+                "username": new
+            }
+    }
+    col.update_one(query, new)
+
+
+def delete_account(email):
+    col = db["Cuenta"]
+    query = {
+        "email": email
+    }
+    return col.delete_one(query)
