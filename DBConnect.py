a65edfdf5761767623b319af37ae41b8395769ee
lluis-vacares <lluis.vacares@estudiantat.upc.edu>
2021-03-23 20:31:16 +0100

    API Delete Account created

--- DBConnect.py
+++ DBConnect.py
@@ -1,18 +1,24 @@
 from flask import Flask, redirect, url_for, request
-
 from src import Cuenta
 
 app = Flask(__name__)
 
 
-@app.route('/create',methods = ['POST'])
+@app.route('/create', methods = ['POST'])
 def Create_account():
-    email = request.form['email']
-    username = request.form['username']
-    password = request.form['password']
-    faction = request.form['faction']
+    email = request.json["email"]
+    username = request.json["username"]
+    password = request.json["password"]
+    faction = request.json["faction"]
     for x in Cuenta.create(email, username, password, faction):
         return x
+
+@app.route('/delete', methods = ['POST'])
+def Delete_account():
+    email = request.json["email"]
+    Cuenta.delete(email)
+    return 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
--- src/Cuenta.py
+++ src/Cuenta.py
@@ -10,30 +10,32 @@ def consult(attribute):
     return col.find({"_id": attribute})
 
 
+
+
 def create(email, name, password, faction):
     aux = 0
-    for x in col.find({"username": name}, { "_id": 0, "username": 1}):
+    for x in col.find({"accountname": name}, { "_id": 0, "accountname": 1}):
         aux = x
     if aux == 0:
         doc = {
             "_id": email,
             "password": password,
-            "username": name,
+            "accountname": name,
             "coins": "0",
             "points": "0",
             "faction": faction
         }
         col.insert_one(doc)
         return consult(email)
-    return None
+    return 500
 
 
-def edit(item, old, new, id):
+def edit(item, new, id):
     if col.find({item: new}) is None:
         new = {"$set": {item: new}}
-        col.update_one({item: old}, {item: new})
+        col.update_one({"_id": id}, {item: new})
         return consult(id)
-    return None
+    return 400
 
 
 def delete(attribute):
