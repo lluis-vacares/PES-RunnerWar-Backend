3d02cd3d99a430c7649405842fb5494831248cf4
Bakanalmi <marc2almi@gmail.com>
2021-03-25 19:06:40 +0100

    edit to update_accountname

--- DBConnect.py
+++ DBConnect.py
@@ -4,7 +4,7 @@ from src import Cuenta
 app = Flask(__name__)
 
 
-@app.route('/create', methods = ['POST'])
+@app.route('/create', methods=['POST'])
 def Create_account():
     email = request.json["email"]
     accountname = request.json["accountname"]
@@ -13,14 +13,16 @@ def Create_account():
     for x in Cuenta.create(email, accountname, password, faction):
         return x
 
-@app.route('/edit', methods = ['POST'])
-def Edit_accountname():
+
+@app.route('/update_accountname', methods=['PUT'])
+def Update_accountname():
     email = request.json["email"]
     accountname = request.json["accountname"]
     for x in Cuenta.edit("accountname", accountname, email):
         return x
 
-@app.route('/delete', methods = ['POST'])
+
+@app.route('/delete', methods=['POST'])
 def Delete_account():
     email = request.json["email"]
     return Cuenta.delete(email)
