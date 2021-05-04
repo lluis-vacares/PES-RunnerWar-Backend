0e747859b6e55b3daacb21867a094a00a0466aa3
lluis-vacares <lluis.vacares@estudiantat.upc.edu>
2021-05-04 20:58:13 +0200

    Lógica de mensaje implementada (crear y mostrar)

--- src/Mensaje.py
+++ src/Mensaje.py
@@ -1,3 +1,5 @@
+import json
+
 import pymongo
 
 client = pymongo.MongoClient(
@@ -7,3 +9,39 @@ db = client["RunnerWar"]
 col = db["Mensaje"]
 
 
+def create_message(sender, receiver, chat, date, time):
+    aux = 0
+    for x in col.find({"sender": sender, "receiver": receiver, "chat": chat, "date": date, "time": time}, {"_id": 0}):
+        aux = x
+    if aux == 0:
+        doc = {
+            "sender": sender,
+            "receiver": receiver,
+            "chat": chat,
+            "date": date,
+            "time": time
+        }
+        col.insert_one(doc)
+        y = {"codi": 200}
+        z = json.dumps(doc)
+        z = json.loads(z)
+        z.update(y)
+        return z
+    else:
+        return {
+            "sender": None,
+            "receiver": None,
+            "chat": None,
+            "date": None,
+            "time": None
+        }
+
+
+def get_all_messages(sender, receiver):
+    a = []
+    for x in col.find({"sender": sender, "receiver": receiver}, {"_id": 0}):
+        a.append(x)
+    for y in col.find({"sender": receiver, "receiver": sender}, {"_id": 0}):
+        a.append(y)
+    js = json.dumps(a)
+    return js
